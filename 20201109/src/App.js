5eab03dca2b1a55834806e8b12738656ceed5618
JJine <51090557+JJine@users.noreply.github.com>
2020-11-09 03:43:14 +0900

    docs: app.js index.js update file

---
+++ 20201109/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Link className="App-link"
+          to="/page"
+        >
+          Learn Page React
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 20201109/src/Page1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page1 = () => {
+    return  (
+        <h3>안녕!</h3>
+    )
+}
+
+export default Page1;
